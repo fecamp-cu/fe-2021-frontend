215476e6593ac9cb0f83787a9d6adb8af6da078f
siradheek <siradhee_k@hotmail.com>
2022-01-04 21:53:46 +0700

    setup router and project structure

---
+++ src/pages/Example/Example.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import "./example.style.css"
+import logo from "../../assets/logo.svg"
+
+const Example = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default Example
+// This is just a proposed project structure, feel free to change it if you want
