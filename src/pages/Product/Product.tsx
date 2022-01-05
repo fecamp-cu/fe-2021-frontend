18606914a83a2f409ad4a7c190769a6f15a8d768
siradheek <siradhee_k@hotmail.com>
2022-01-05 20:50:19 +0700

    add glass container to global css

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -5,7 +5,7 @@ import Book from "../../assets/book_cover.jpg"
 const testProps: ProductInfoProps = {
   title: "เตรียมสอบ PAT3 ความถนัดทางวิศวกรรมศาสตร์",
   summary:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo velit, dolorum aliquam excepturi quos eligendi doloribus omnis voluptate cumque assumenda iure ab minima, quasi, maiores quas impedit ad dolore exercitationem.",
+    "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe iure velit architecto asperiores obcaecati temporibus perspiciatis tempora quos, quae ex laudantium nam, commodi in rem fugit at magni nobis veritatis cum expedita aliquid distinctio a? Corrupti commodi mollitia blanditiis quasi officia nihil recusandae magni rerum quisquam adipisci, modi itaque dignissimos expedita! Ipsa vitae consequuntur porro earum quia vel molestias, eum dignissimos itaque nihil nostrum quo architecto iure vero molestiae cum quam minima, eligendi reprehenderit officiis nisi. Et suscipit quod incidunt soluta! Sapiente quibusdam eos architecto! Nam ipsa aliquid ut, eligendi nostrum quae non laboriosam tenetur. Maxime libero voluptatem ex",
   type: "examPreps",
   author: "Somedude WithSurname",
   thumbnail: Book,
@@ -14,7 +14,7 @@ const testProps: ProductInfoProps = {
 }
 const Product = () => {
   return (
-    <div className="container mx-auto glass-container p-2">
+    <div className="container mx-auto glass-container p-4 md:pt-8">
       <ProductInfo {...testProps} />
     </div>
   )
