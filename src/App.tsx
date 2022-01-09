0454c411f1d9dbf0133825385aee1469369382c1
Nitiwat-owen <77772598+Nitiwat-owen@users.noreply.github.com>
2022-01-09 14:04:50 +0700

    Add Form
    Add Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Example from "./pages/Example/Example"
+import Form from "./components/Form/Form"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Example />}></Route>
+      <Route path="/" element={<Form />}></Route>
     </Routes>
   )
 }
