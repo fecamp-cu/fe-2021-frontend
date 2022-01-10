b8a662f198e6cc6c0aecb31fed61d48fdc63024a
MoBiEeW <mobiew55@gmail.com>
2022-01-10 22:35:46 +0700

    (v0.99)
    Many bugs have been fixed. but not yet added style-component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 import "./Navbar.css"
 import "../../index.css"
-import { useLocation,Link } from "react-router-dom"
-import {MdLaunch} from "react-icons/md"
-import {HiMenuAlt3} from "react-icons/hi"
+import { useLocation, Link } from "react-router-dom"
+import { MdLaunch } from "react-icons/md"
+import { HiMenuAlt3 } from "react-icons/hi"
 
 interface NavbarProps {
   userName: string
@@ -14,82 +14,32 @@ function Navbar(props: NavbarProps) {
   const { userName, isLogin } = props
   const location = useLocation()
   const atPage = location.pathname
-  
+
   /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown")!.classList.toggle("show");
-}
+  function dropMenu() {
+    document.getElementById("Menu-list")!.classList.toggle("show")
+  }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event:any) {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  function dropProfile() {
+    document.getElementById("profile-list")!.classList.toggle("show")
+  }
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function (event: any) {
+    if (!event.target.matches(".dropbtn")) {
+      const dropdowns = document.getElementsByClassName("dropdown-content")
+      let i
+      for (i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i]
+        if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show")
+        }
       }
     }
   }
-}
 
-// return(
-//   <div className="dropdown">
-//   <button onClick={myFunction} className="navbar-toggler dropbtn"><HiMenuAlt3 color="white" size="2.5rem"/></button>
-//   <div className="dropdown-menu dropdown-content" style={{ background: "var(--gm-color)", width: "100%", margin: "10px 0px" }} id="myDropdown">
-//   <li className="dropdown-item">
-//             <Link className={""} to={"/"} id="element">
-//               หน้าแรก
-//             </Link>
-//           </li>
-//           <hr></hr>
-//           <li className="dropdown-item">
-//             <Link className={""} to={"/shop"} id="element">
-//               ร้านหนังสือ
-//             </Link>
-//           </li>
-//           <hr></hr>
-//           {isLogin ? (
-//             <>
-//             <li className="dropdown-item">
-//               <Link className={""} to={"/lesson"} id="element">
-//                 เข้าสู่บทเรียน <MdLaunch/>
-//               </Link>
-//             </li>
-//             <hr></hr>
-//             </>
-//           ) : (
-//             <></>
-//           )}
-        
-//           {isLogin ? (
-//             <>
-//               <li className="dropdown-item">
-//                 <Link className={""} id="element" to={"/profile"}>
-//                   ข้อมูลส่วนตัว
-//                 </Link>
-//               </li>
-//               <hr></hr>
-//               <li className="dropdown-item">
-//                 <Link className={""} id="element" to={"/logout"}>
-//                   ออกจากระบบ
-//                 </Link>
-//               </li>
-//             </>
-//           ) : (
-//             <>
-//               <li className="dropdown-item">
-//                 <Link className={""} id="element" to={"/login"}>
-//                   เข้าสู่ระบบ
-//                 </Link>
-//               </li>
-//             </>
-//           )}
-//   </div>
-// </div>
-// )
+  const style = {display:"block"}
 
   return (
     <nav
@@ -103,14 +53,17 @@ window.onclick = function(event:any) {
           Navbar
         </a>
 
-        
-          {/* --------------toggle button, it will appear when open in mobile------------------ */}
-        <button className="navbar-toggler dropbtn" type="button" onClick={myFunction}>
-          <HiMenuAlt3 color="white" size="2.5rem"/>
+        {/* --------------toggle button, it will appear when open in mobile------------------ */}
+        <button className="navbar-toggler dropbtn" type="button" onClick={dropMenu}>
+          <HiMenuAlt3 color="white" size="2.5rem" className="dropbtn" />
         </button>
 
         {/* -------------------the menu dropdown when you open in mobile--------------------- */}
-        <div className="dropdown-menu dropdown-content" style={{ background: "var(--gm-color)", width: "100%", margin: "10px 0px" ,top:"100%",left:"0px"}} id="myDropdown">
+        <div
+          className="dropdown-menu dropdown-content"
+          style={{ background: "var(--gm-color)", width: "100%", margin: "10px 0px", top: "100%", left: "0px" }}
+          id="Menu-list"
+        >
           <li className="dropdown-item">
             <Link className={""} to={"/"} id="element">
               หน้าแรก
@@ -125,17 +78,17 @@ window.onclick = function(event:any) {
           <hr></hr>
           {isLogin ? (
             <>
-            <li className="dropdown-item">
-              <Link className={""} to={"/lesson"} id="element">
-                เข้าสู่บทเรียน <MdLaunch/>
-              </Link>
-            </li>
-            <hr></hr>
+              <li className="dropdown-item">
+                <Link className={""} to={"/lesson"} id="element">
+                  เข้าสู่บทเรียน<MdLaunch/>
+                </Link>
+              </li>
+              <hr></hr>
             </>
           ) : (
             <></>
           )}
-        
+
           {isLogin ? (
             <>
               <li className="dropdown-item">
@@ -161,8 +114,6 @@ window.onclick = function(event:any) {
           )}
         </div>
 
-       
-
         {/* -------------------------the normal menu, when you open in desktop---------------------- */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav list-inline me-auto justify-content-star">
@@ -179,7 +130,7 @@ window.onclick = function(event:any) {
             {isLogin ? (
               <li className="nav-item">
                 <Link className={"pt-1 me-4" + (atPage === "/lesson" ? " border-bottom" : "")} to={"/lesson"} id="element">
-                  เข้าสู่บทเรียน <MdLaunch/>
+                  เข้าสู่บทเรียน <MdLaunch />
                 </Link>
               </li>
             ) : (
@@ -196,20 +147,20 @@ window.onclick = function(event:any) {
               <>
                 <img className="rounded-circle d-inline-flex me-2" src="" width={50} height={50} alt="" id="picture"></img>
                 <li className="nav-item dropdown show">
-                  <a className={"pt-2 me-4 d-inline-flex"} data-bs-toggle="dropdown" id="profile">
+                  <button className={"pt-2 me-4 d-inline-flex dropbtn"} data-bs-toggle="dropdown" id="profile" onClick={dropProfile}>
                     {userName}
-                  </a>
-                  <ul className="dropdown-menu mt-3" style={{ background: "var(--gm-color)", margin: "0% 0% 0% -45%" }} id="border-drop">
-                    <li>
-                      <Link className="dropdown-item ms-2" to={"/profile"}>
-                        ข้อมูลส่วนตัว
-                      </Link>
-                      <hr></hr>
-                      <Link className="dropdown-item ms-2" to={"/logout"}>
-                        ออกจากระบบ
-                      </Link>
-                    </li>
-                  </ul>
+                  </button>
+                    <ul className="dropdown-menu dropdown-content border-drop" style={{ background: "var(--gm-color)", margin: "30px 0% 0% -60px", width:"176px", height:"86px" }} id="profile-list">
+                      <li>
+                        <Link className="dropdown-item center" to={"/profile"}>
+                          ข้อมูลส่วนตัว
+                        </Link>
+                        <hr></hr>
+                        <Link className="dropdown-item center" to={"/logout"}>
+                          ออกจากระบบ
+                        </Link>
+                      </li>
+                    </ul>
                 </li>
               </>
             ) : (
