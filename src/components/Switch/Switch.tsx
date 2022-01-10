7acc0124ecd80dbb48c91bafc1b8fe0c63190e37
siradheek <siradhee_k@hotmail.com>
2022-01-11 00:10:20 +0700

    make switch resuable

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,21 +1,35 @@
 import * as React from "react"
+import { useState, useCallback } from "react"
 import "./switch.style.css"
-// interface TabItem {
-//   title: string
-//   onClick?: (title: string) => void
-// }
-interface SwitchProps {
-  items: string[]
-  selectedItem?: string
+interface SwitchItem {
+  title: string
+  value: string
 }
-const Switch = ({ items, selectedItem }: SwitchProps) => {
+interface SwitchProps {
+  items: SwitchItem[]
+  onSelectCallback: (value: string) => void
+}
+const Switch = ({ items, onSelectCallback }: SwitchProps) => {
+  const [selectedItem, setSelectedItem] = useState(items[0])
+  const onSelect = useCallback(
+    (item: SwitchItem) => {
+      setSelectedItem(item)
+      onSelectCallback(item.value)
+    },
+    [onSelectCallback]
+  )
   return (
     <div className="flex justify-center">
       <div className="flex switch">
         {items.map((item) => (
-          <div className={`flex justify-center items-center px-5 py-2`} id={selectedItem === item ? "selected-tab" : ""}>
-            {item}
-          </div>
+          <button
+            className={`flex justify-center items-center px-5 py-2`}
+            onClick={() => onSelect(item)}
+            key={item.title}
+            id={selectedItem.title === item.title ? "selected-tab" : ""}
+          >
+            {item.title}
+          </button>
         ))}
       </div>
     </div>
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,26 +1,54 @@
-import React from "react"
+import React, { useMemo, useState } from "react"
 import "./product.style.css"
 import { ProductInfo, ProductInfoProps } from "../../components/ProductInfo/ProductInfo"
 import Book from "../../assets/book_cover.jpg"
 import Switch from "../../components/Switch/Switch"
-const testProps: ProductInfoProps = {
-  title: "เตรียมสอบ PAT3 ความถนัดทางวิศวกรรมศาสตร์",
-  summary:
-    "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe iure velit architecto asperiores obcaecati temporibus perspiciatis tempora quos, quae ex laudantium nam, commodi in rem fugit at magni nobis veritatis cum expedita aliquid distinctio a? Corrupti commodi mollitia blanditiis quasi officia nihil recusandae magni rerum quisquam adipisci, modi itaque dignissimos expedita! Ipsa vitae consequuntur porro earum quia vel molestias, eum dignissimos itaque nihil nostrum quo architecto iure vero molestiae cum quam minima, eligendi reprehenderit officiis nisi. Et suscipit quod incidunt soluta! Sapiente quibusdam eos architecto! Nam ipsa aliquid ut, eligendi nostrum quae non laboriosam tenetur. Maxime libero voluptatem ex",
-  type: "examPreps",
-  author: "Somedude WithSurname",
-  thumbnail: Book,
-  index: ["ปรนัยตัวเลือก 10 ข้อ", "ปรนัยตัวเลือก 5 ข้อ", "เติมคำ 5 ข้อ"],
-  price: 199,
-  fileUrl: "https://pdfgeneratorapi.com/example-documents/52272/pdf",
-}
+const testProps: ProductInfoProps[] = [
+  {
+    title: "เตรียมสอบ PAT3 ความถนัดทางวิศวกรรมศาสตร์",
+    summary:
+      "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe iure velit architecto asperiores obcaecati temporibus perspiciatis tempora quos, quae ex laudantium nam, commodi in rem fugit at magni nobis veritatis cum expedita aliquid distinctio a? Corrupti commodi mollitia blanditiis quasi officia nihil recusandae magni rerum quisquam adipisci, modi itaque dignissimos expedita! Ipsa vitae consequuntur porro earum quia vel molestias, eum dignissimos itaque nihil nostrum quo architecto iure vero molestiae cum quam minima, eligendi reprehenderit officiis nisi. Et suscipit quod incidunt soluta! Sapiente quibusdam eos architecto! Nam ipsa aliquid ut, eligendi nostrum quae non laboriosam tenetur. Maxime libero voluptatem ex",
+    type: "examPreps",
+    author: "Somedude WithSurname",
+    thumbnail: Book,
+    index: ["ปรนัยตัวเลือก 10 ข้อ", "ปรนัยตัวเลือก 5 ข้อ", "เติมคำ 5 ข้อ"],
+    price: 199,
+    fileUrl: "https://pdfgeneratorapi.com/example-documents/52272/pdf",
+  },
+  {
+    title: "ข้อสอบเตรียมสอบ PAT3 ความถนัดทางวิศวกรรมศาสตร์",
+    summary:
+      "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Non saepe iure velit architecto asperiores obcaecati temporibus perspiciatis tempora quos, quae ex laudantium nam, commodi in rem fugit at magni nobis veritatis cum expedita aliquid distinctio a? Corrupti commodi mollitia blanditiis quasi officia nihil recusandae magni rerum quisquam adipisci, modi itaque dignissimos expedita! Ipsa vitae consequuntur porro earum quia vel molestias, eum dignissimos itaque nihil nostrum quo architecto iure vero molestiae cum quam minima, eligendi reprehenderit officiis nisi. Et suscipit quod incidunt soluta! Sapiente quibusdam eos architecto! Nam ipsa aliquid ut, eligendi nostrum quae non laboriosam tenetur. Maxime libero voluptatem ex",
+    type: "oldPapers",
+    author: "Somedude WithSurname",
+    thumbnail: Book,
+    index: ["ปรนัยตัวเลือก 10 ข้อ", "ปรนัยตัวเลือก 5 ข้อ", "เติมคำ 5 ข้อ"],
+    price: 199,
+    fileUrl: "https://pdfgeneratorapi.com/example-documents/226340/pdf",
+  },
+]
 const Product = () => {
+  const switchItems = [
+    {
+      title: "หนังสือเตรียมสอบ",
+      value: "examPreps",
+    },
+    {
+      title: "ข้อสอบเก่า",
+      value: "oldPapers",
+    },
+  ]
+  const [product, setProduct] = useState<ProductInfoProps>(testProps[0])
+  const getProduct = (value: string) => {
+    //api calls?
+    if (value === "oldPapers") setProduct(testProps[1])
+    else setProduct(testProps[0])
+  }
   return (
     <>
-      <Switch items={["หนังสือหนังหา", "ข้อส่งข้อสอบ"]} selectedItem="หนังสือหนังหา" />
-
+      <Switch items={switchItems} onSelectCallback={getProduct} />
       <div className="mx-4 sm:mx-12 glass-container p-4 md:pt-8 mt-5">
-        <ProductInfo {...testProps} />
+        <ProductInfo {...product} />
       </div>
     </>
   )
