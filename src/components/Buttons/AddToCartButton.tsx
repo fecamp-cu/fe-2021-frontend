2d69ff8f38ee17cb9eed2889b8aa402906421c5a
abcdeclair <88878365+abcdeclair@users.noreply.github.com>
2022-01-10 23:30:43 +0700

    buttons (edit but not finished)

--- src/components/Buttons/AddToCartButton.tsx
+++ src/components/Buttons/AddToCartButton.tsx
@@ -1,17 +1,82 @@
 import React from "react";
+import styled from 'styled-components';
 import './AddToCartButton.style.css';
 import { IoMdBasket } from 'react-icons/io';
 
 
-interface AddToCartButtonProps {
-    onClick: (e:React.MouseEvent<HTMLButtonElement>) => void
+const Button = styled.button`
+  position: absolute;
+  width: 395px;
+  height: 40px;
+
+  background: #ab3a41;
+  border-radius: 5px;
+  border: 0px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  padding-top: 10px;
+
+  @media (max-width: 390px) {
+    position: absolute;
+    width: 330px;
+    height: 30px;
 
   }
+`;
 
-  const AddToCartButton: React.FC<AddToCartButtonProps>=({onClick })=> {
+const WhiteText = styled.a`
+  position: absolute;
+  font-family: CHULALONGKORN;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 30px;
+  /* identical to box height */
 
-    return <button className="addToCartButton" onClick={onClick}><p className="whiteNormalText"><IoMdBasket className="img"></IoMdBasket>เพิ่มลงตะกร้า</p></button>
+  display: flex;
+  align-items: center;
+  text-align: right;
+
+  color: #ffffff;
+
+  @media (max-width: 390px) {
+    position: absolute;
+    font-size: 14px;
+    line-height: 21px;
+
+  }
+`;
+
+const Logo = styled.img`
+  margin-right: 10px;
+  margin-bottom: 10px;
+  
+`;
+
+interface AddToCartButtonProps {
+  onClick: any
 
 }
 
-  export default AddToCartButton
+const AddToCartButton: React.FC<AddToCartButtonProps> = ({ onClick }) => {
+
+  return <Button onClick={onClick}><WhiteText><Logo><IoMdBasket></IoMdBasket></Logo>เพิ่มลงตะกร้า</WhiteText></Button>
+
+}
+
+// interface AddToCartButtonProps {
+//     onClick: (e:React.MouseEvent<HTMLButtonElement>) => void
+
+//   }
+
+//   const AddToCartButton: React.FC<AddToCartButtonProps>=({onClick })=> {
+
+//     return <button className="addToCartButton" onClick={onClick}><p className="whiteNormalText"><IoMdBasket className="img"></IoMdBasket>เพิ่มลงตะกร้า</p></button>
+
+// }
+
+export default AddToCartButton
