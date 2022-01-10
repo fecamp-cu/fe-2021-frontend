59ee9bbaf535e2be9800e1ac9342e2b9ae3a8b00
samithiwat <samithiwat2544@gmail.com>
2022-01-11 00:56:50 +0700

    Bring back the App component in the index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
+import Footer from "./components/Footer/Footer"
 import Example from "./pages/Example/Example"
 import Product from "./pages/Product/Product"
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Example />}></Route>
       <Route path="/product" element={<Product />}></Route>
+      <Route path="/footer" element={<Footer />}></Route>
     </Routes>
   )
 }
