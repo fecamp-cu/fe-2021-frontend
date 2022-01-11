727430ec7d14f7e907f99ace699a2aa16389804a
MoBiEeW <mobiew55@gmail.com>
2022-01-11 16:36:12 +0700

    init payment page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom"
 import Example from "./pages/Example/Example"
 import Navbar from "./components/Navbar/Navbar"
 import Product from "./pages/Product/Product"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <>
     <Navbar isLogin={true}/>
+    
     <Routes>
       <Route path="/" element={<Example />}></Route>
       <Route path="/product" element={<Product />}></Route>
     </Routes>
+    
+    <Footer />
+    
     </>
   )
 }
