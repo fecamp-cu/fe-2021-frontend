dc6ff7f80971b48f6f6d71fa11a3713fe84d7e5f
MoBiEeW <mobiew55@gmail.com>
2022-01-11 16:51:31 +0700

    update

--- src/pages/Payment/Payment.tsx
+++ src/pages/Payment/Payment.tsx
@@ -1,1 +1,15 @@
 import React from "react"
+import styled from 'styled-components';
+
+
+const paymentComponentBg = styled({padding:"1px"})
+
+
+function payment(){
+    return (
+        <div>
+
+        </div>
+    )
+
+}
