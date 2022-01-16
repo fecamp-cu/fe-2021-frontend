aa049ebc8517dc6c88f50ea936b1316f9f60c32e
abcdeclair <88878365+abcdeclair@users.noreply.github.com>
2022-01-16 11:04:43 +0700

    Button (add some style to applyButton)

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,17 @@ import Example from "./pages/Example/Example"
 import Navbar from "./components/Navbar/Navbar"
 import Product from "./pages/Product/Product"
 import Profile from "./components/Profile_picture/Profile"
-import {Button} from "./components/Buttons/Button"
-import { IoMdBasket } from 'react-icons/io';
-import ApplyButton from "./components/Buttons/ApplyButton"
 
 function App() {
   return (
     <>
       <Navbar isLogin={true} />
-      <Button width="433" bg="white" textColor="linear-gradient(93.44deg, #af3b43 100%, #ea727f 100%)" outline={false} shadow>เข้าสู่ระบบ</Button>
-      
-      <Button width="335" bg="white" textColor="linear-gradient(93.44deg, #af3b43 100%, #ea727f 100%)" outline={false} shadow>ลงทะเบียน</Button>
-      
-      <Button width="335" textColor="white" outline shadow={false}>ยกเลิก</Button>
-      
-      <Button width="395" textColor="white" outline shadow={false}>ดูตัวอย่างหนังสือ</Button>
-      
-      <Button width="395" bg="#AB3A41" textColor="white" outline={false} shadow icon={<IoMdBasket/>} fontWeight="Normal">เพิ่มลงตะกร้า</Button>
-      
-      <Button width="809" bg="white" textColor="linear-gradient(93.44deg, #af3b43 100%, #ea727f 100%)" outline={false} shadow>ลงทะเบียน</Button>
-      
-      <Button width="809" outline shadow={false}>ดูตัวอย่างข้อสอบ</Button>
-      
-      <Button width="454" bg="white" textColor="linear-gradient(93.44deg, #af3b43 100%, #ea727f 100%)" outline={false} shadow>ยืนยันและไปชำระเงิน</Button>
-      
-      <Button width="71" bg="#D45161" fontSize="16px" lineHeight="24px" outline={false} shadow>ใช้โค้ด</Button>
-      
-      <Button width="207" textColor="white" outline shadow={false}>ไปหน้าข้อมูลผู้ใช้</Button>
-      
-      <Button width="207" bg="white" textColor="linear-gradient(93.44deg, #af3b43 100%, #ea727f 100%)" outline={false} shadow>กลับหน้าหลัก</Button>
-      
-      <ApplyButton></ApplyButton>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Example />}></Route>
         <Route path="/product" element={<Product />}></Route>
         <Route path="/footer" element={<Footer />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
-      </Routes> */}
+      </Routes>
     </>
   )
 }
--- src/components/Buttons/ApplyButton.tsx
+++ src/components/Buttons/ApplyButton.tsx
@@ -15,25 +15,38 @@ const ApplyBtn = styled.button`
   text-align: center;
   justify-content: center;
   padding-top: 10px; 
-  transition: opacity 3s ease-in-out;
+
+  color: #ffffff;
+  
 
   @media (max-width: 390px) {
       width: 148px;
       height: 49px;
     }
+  
    &:hover{
        background: linear-gradient(262.9deg, #C43E56 8.25%, #D24F66 29.21%, #D7525B 71.52%, #C83A41 94.65%);
-       transition: opacity 3s ease-in-out;
-       animation:slidebg 2s infinite;
-       animation-fill-mode: both;
-       
-       
+       animation:slidebg 0.5s infinite;
+       animation-fill-mode: forwards;
    }
 
-    @keyframes slidebg {
-        
-        to {background: linear-gradient(262.9deg, #C83A41 8.25%, #D7525B 29.21%, #D24F66 71.52%, #C43E56 94.65%);}
+    &:active{
+      background: #FFFFFF;
+      & > button {
+         background: #FFFFFF;
       }
+      & > p {
+        display: flex;
+        justify-content: center;
+        background: linear-gradient(93.44deg, #af3b43 100%, #ea727f 100%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+      }
+    }
+    @keyframes slidebg {
+      to {background: linear-gradient(262.9deg, #C83A41 8.25%, #D7525B 29.21%, #D24F66 71.52%, #C43E56 94.65%);}
+    }
+
 }`;
 
 const ApplyTxt = styled.p`
@@ -46,7 +59,6 @@ const ApplyTxt = styled.p`
 
   display: flex;
 
-  color: #ffffff;
 
   @media (max-width: 390px){
     font-size: 28px;
