e860ad31b0694efd79abf31121aeb770e1225c0b
siradheek <siradhee_k@hotmail.com>
2022-01-16 16:00:09 +0700

    add api calls

--- src/utils/checkoutOmise.ts
+++ src/utils/checkoutOmise.ts
@@ -1,11 +1,18 @@
-export const checkoutOmise = (amount: number) => {
+import { AxiosResponse } from "axios"
+import { client } from "./client"
+export const checkoutOmise = (amount: number, order: any, onCheckoutSuccess: (res: AxiosResponse) => void) => {
   window.OmiseCard.open({
     amount: amount * 100,
     currency: "THB",
     defaultPaymentMethod: "credit_card",
-    onCreateTokenSuccess: (nonce: string) => {
-      //console.log(nonce)
-      // api calls
+    onCreateTokenSuccess: async (nonce: string) => {
+      order.token = nonce
+      try {
+        const res = await client.post("/shop/checkout", order)
+        onCheckoutSuccess(res)
+      } catch (err) {
+        //handle error
+      }
     },
   })
 }
