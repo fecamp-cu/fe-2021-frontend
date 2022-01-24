55ef1e032293ce793b8b3cb61a8362e730e00822
abcdeclair <88878365+abcdeclair@users.noreply.github.com>
2022-01-24 21:51:41 +0700

    Update Logo.tsx

--- src/components/Buttons/Logo.tsx
+++ src/components/Buttons/Logo.tsx
@@ -6,11 +6,13 @@ export interface LogoProps {
     marginright? : string
     marginleft?: string
     marginbottom?: string
+    margintop?: string
 }
 
 export const Logo = styled.span<LogoProps>`
   margin-right: ${(props) => props.marginright || "10px"};
   margin-left: ${(props) => props.marginleft || "0px"};
   margin-bottom: ${(props) => props.marginbottom || "10px"};
+  margin-top: ${(props) => props.margintop || "10px"};
   color: #fff;
 }`;
