16061a8fcafdf2e63f3e69c645f4bc61192ac37f
abcdeclair <88878365+abcdeclair@users.noreply.github.com>
2022-01-24 21:49:50 +0700

    edit button

---
+++ src/components/Buttons/Logo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from 'prop-types'
+
+export interface LogoProps {
+    marginright? : string
+    marginleft?: string
+    marginbottom?: string
+}
+
+export const Logo = styled.span<LogoProps>`
+  margin-right: ${(props) => props.marginright || "10px"};
+  margin-left: ${(props) => props.marginleft || "0px"};
+  margin-bottom: ${(props) => props.marginbottom || "10px"};
+  color: #fff;
+}`;
