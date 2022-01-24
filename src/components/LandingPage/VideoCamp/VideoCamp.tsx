43b41887753a00a2079d4c3c2284e573c8f0f14e
Pisit Wongsripisant <act21078@gmail.com>
2022-01-24 19:43:10 +0700

    edit video camp component

--- src/components/LandingPage/VideoCamp/VideoCamp.tsx
+++ src/components/LandingPage/VideoCamp/VideoCamp.tsx
@@ -4,7 +4,7 @@ function VideoCamp() {
     return (
         <div className="video-container">
             <div className="video-text">คลิปเปิดค่าย Jailbreak by FeCampX - FECamp Chula</div>
-            <iframe className="video-camp" src="https://www.youtube.com/embed/0OCyTBaPiYc"></iframe>
+            <iframe className="video-camp" src="https://www.youtube.com/embed/0OCyTBaPiYc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
