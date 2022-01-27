d5b63729398f4044c81f8959634a2cbb798115c2
samithiwat <samithiwat2544@gmail.com>
2022-01-28 06:07:43 +0700

    Add more example about calling the API

--- src/pages/Example/Example.tsx
+++ src/pages/Example/Example.tsx
@@ -1,10 +1,7 @@
 import React from "react"
-import logo from "../../assets/logo.svg"
-import AlertButton from "../../components/AlertButton/AlertButton"
 import { Button } from "../../components/Buttons/Button"
-import { default as axiosInstance, default as clientInstance } from "../../utils/client"
-import { PaymentTypes, PromotionCodeType } from "../../utils/enums"
-import omiseInstance from "../../utils/omise"
+import { default as clientInstance } from "../../utils/client"
+import { AboutFe, Photo, Qualification, Sponsor, Timeline } from "../../utils/types/setting"
 import "./example.style.css"
 
 const basket = [{ productId: 1, quantity: 1 }]
@@ -28,74 +25,44 @@ const costumerData = {
 }
 
 const Example = () => {
-  const [token, setToken] = React.useState<object>({})
-  const [source, setSource] = React.useState<object>({})
-  const [code, setCode] = React.useState<string>("")
+  const [youtube, setYoutube] = React.useState<string>("")
+  const [register, setRegister] = React.useState<string>("")
+  const [photos, setPhoto] = React.useState<Photo[]>([])
+  const [aboutFEs, setAboutFE] = React.useState<AboutFe[]>([])
+  const [timelines, setTimeline] = React.useState<Timeline[]>([])
+  const [qualifications, setQualification] = React.useState<Qualification[]>([])
+  const [sponsors, setSponsor] = React.useState<Sponsor[]>([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button shadow={false} outline={false} onClick={() => omiseInstance.createOmiseToken(10000, setToken)}>
-          Create Token
+        <Button shadow={false} outline={false} onClick={() => clientInstance.login()}>
+          Login
         </Button>
-        <Button shadow={false} outline={false} onClick={() => console.log(token)}>
-          Check Token
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => axiosInstance.checkout(token, PaymentTypes.cardEndpoint, costumerData, basket)}>
-          Send Token to Backend
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => omiseInstance.createSourceOmise(10000, "promptpay", setSource)}>
-          Promptpay Source
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => console.log(source)}>
-          Check Source
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => axiosInstance.checkout(source, PaymentTypes.promptPayEndpoint, costumerData, basket)}>
-          Send Source to Backend
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => omiseInstance.createSourceOmise(10000, "internet_banking_scb", setSource)}>
-          Internet Banking Source
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => console.log(source)}>
-          Check Source
+        <Button shadow={false} outline={false} onClick={() => clientInstance.userInfo()}>
+          User Info
         </Button>
         <Button
           shadow={false}
           outline={false}
-          onClick={async () => {
-            const res = await axiosInstance.checkout(source, PaymentTypes.eBankEndpoint, costumerData, basket)
-            window.open(res.authorize_uri, "_blank")
+          onClick={() => clientInstance.getActivedSetting(setYoutube, setRegister, setAboutFE, setTimeline, setPhoto, setQualification, setSponsor)}
+        >
+          Settings
+        </Button>
+        <Button
+          shadow={false}
+          outline={false}
+          onClick={() => {
+            console.log(youtube)
+            console.log(register)
+            console.log(photos)
+            console.log(aboutFEs)
+            console.log(timelines)
+            console.log(qualifications)
+            console.log(sponsors)
           }}
         >
-          Send Source to Backend
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => axiosInstance.testAPI()}>
-          Test
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => axiosInstance.genCode(PromotionCodeType.AMOUNT, setCode)}>
-          Generate Code Amount
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => axiosInstance.genCode(PromotionCodeType.PERCENTAGE, setCode)}>
-          Generate Code Percentage
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => console.log(code)}>
-          Check Code
-        </Button>
-        <Button shadow={false} outline={false} onClick={() => axiosInstance.verifyCode(code)}>
-          Verify Code
-        </Button>
-        <br />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <AlertButton isEmergency message="This is just a proposed project structure, feel free to change it if you want">
-          Example Button
-        </AlertButton>
-        <Button shadow={false} outline={false} onClick={() => clientInstance.testAPI()}>
-          Test
+          Log all setting
         </Button>
       </header>
     </div>
--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,54 +1,9 @@
-import Axios from "axios"
+import { AxiosResponse, default as axios, default as Axios } from "axios"
+import { addSeconds, isPast } from "date-fns"
 import { PaymentTypes, PromotionCodeType } from "./enums"
-
-export type GenPromotionCode = {
-  type: PromotionCodeType
-  isReuseable?: boolean
-  expireDate?: Date
-  code?: string
-  value?: number
-}
-
-export type costumerData = {
-  firstName: string
-  lastName: string
-  tel: string
-  email: string
-  grade: string
-  school: string
-  address: string
-  subdistrict: string
-  district: string
-  province: string
-  postcode: string
-  shippingAddress: string
-  shippingSubDistrict: string
-  shippingDistrict: string
-  shippingProvince: string
-  shippingPostCode: string
-}
-
-export type Basket = {
-  productId: number
-  quantity: number
-}
-
-export type CheckoutPayload = {
-  source: object
-  email: string
-  firstName: string
-  lastName: string
-  tel: string
-  grade: string
-  school: string
-  address: string
-  subdistrict: string
-  district: string
-  province: string
-  postcode: string
-  basket: Basket[]
-  promotionCode?: string
-}
+import { Credentials } from "./types/auth"
+import { AboutFe, Photo, Qualification, Setting, Sponsor, Timeline } from "./types/setting"
+import { Basket, CheckoutPayload, costumerData, GenPromotionCode } from "./types/shop"
 
 function createCheckoutPayload(source: object, costumerData: costumerData, basket: Basket[], promotionCode?: string): CheckoutPayload {
   const data: CheckoutPayload = {
@@ -82,9 +37,77 @@ function createCheckoutPayload(source: object, costumerData: costumerData, baske
   return data
 }
 
+function storeToken(credentials: Credentials): void {
+  const expireDate = addSeconds(Date.now(), credentials.expiresIn).toISOString()
+
+  localStorage.setItem("fe_camp_access_token", credentials.accessToken)
+  localStorage.setItem("fe_camp_refresh_token", credentials.refreshToken)
+  localStorage.setItem("fe_camp_expire_date", expireDate)
+}
+
 const client = Axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
 })
+
+export const getCSRFToken = async () => {
+  const res: AxiosResponse = await axios.get(process.env.REACT_APP_API_URL + "/who-am-i")
+  console.log(res.data)
+  client.defaults.headers.common["XSRF-Token"] = res.data.csrfToken
+}
+
+async function addBearer() {
+  let accessToken = localStorage.getItem("fe_camp_access_token")
+
+  if (isPast(new Date(localStorage.getItem("fe_camp_expire_date") as string))) {
+    const res: AxiosResponse = await client.post("/auth/token", { refreshToken: localStorage.getItem("fe_camp_refresh_token") as string })
+    accessToken = res.data.accessToken
+    storeToken(res.data)
+  }
+
+  client.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`
+}
+
+const login = async () => {
+  const res = await client.post("/auth/login", {
+    email: "superadmin@gmail.com",
+    password: "adminadmin",
+  })
+
+  storeToken(res.data)
+}
+
+const userInfo = async () => {
+  await addBearer()
+  const res = await client.get("/auth/me", {
+    withCredentials: true,
+  })
+  console.log(res)
+}
+
+const getActivedSetting = async (
+  setYoutubeURL: (url: string) => void,
+  setRegisterURL: (url: string) => void,
+  setAboutFEs: (aboutFEs: AboutFe[]) => void,
+  setTimelines: (timelines: Timeline[]) => void,
+  setPhotos: (photos: Photo[]) => void,
+  setQualifications: (qualifications: Qualification[]) => void,
+  setSponsors: (sponsors: Sponsor[]) => void
+): Promise<void> => {
+  await addBearer()
+  const res = await client.get("/settings/active")
+  const activedSetting: Setting = res.data
+
+  setYoutubeURL(activedSetting.youtubeUrl)
+  setRegisterURL(activedSetting.registerFormUrl)
+  setAboutFEs(activedSetting.aboutFeContainers)
+  setTimelines(activedSetting.timelineEvents)
+  setPhotos(activedSetting.photoPreviews)
+  setQualifications(activedSetting.qualificationPreviews)
+  setSponsors(activedSetting.sponcerContainers)
+
+  console.log(res)
+}
 
 const checkout = async (source: object, paymentType: PaymentTypes, costumerData: costumerData, basket: Basket[], promotionCode?: string) => {
   const data = createCheckoutPayload(source, costumerData, basket, promotionCode)
@@ -147,6 +170,9 @@ const clientInstance = {
   checkout,
   genCode,
   verifyCode,
+  login,
+  userInfo,
+  getActivedSetting,
 }
 
 export default clientInstance
