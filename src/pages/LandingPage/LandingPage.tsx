40d169b605269d3fd0e27e754605ac4668907b25
samithiwat <samithiwat2544@gmail.com>
2022-02-21 05:08:10 +0700

    Rework Landing Page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,67 +1,71 @@
-import About from "../../components/LandingPage/About/About"
-import ApplyButton from "../../components/Buttons/ApplyButton";
-import Qualifications from "../../components/LandingPage/Qualifications/Qualifications"
-import Schedule from "../../components/LandingPage/Schedule/Schedule";
-import Sponsor from "../../components/LandingPage/Sponsor/Sponsor"
-import icon from "../../assets/images/Icon.png"
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import About from "../../components/LandingPage/About"
+import ApplyButton from "../../components/Buttons/ApplyButton"
+import Qualifications from "../../components/LandingPage/Qualifications"
+import Schedule from "../../components/LandingPage/Schedule"
+import Sponsor from "../../components/LandingPage/Sponsor"
+import AnchorLink from "react-anchor-link-smooth-scroll"
 
-import "./LandingPage.css"
-import PhotoPreview from "../../components/LandingPage/PhotoPreview/PhotoPreview";
-import VideoCamp from "../../components/LandingPage/VideoCamp/VideoCamp";
-import { Link } from "react-router-dom";
-import Navbar from "../../components/Navbar/Navbar";
-import { useEffect, useState } from "react";
-import { landingPageInstance } from "../../utils/client";
+import PhotoPreview from "../../components/LandingPage/PhotoPreview"
+import VideoCamp from "../../components/LandingPage/VideoCamp"
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { apiClient } from "../../utils/client"
 import star from "../../assets/images/star.svg"
+import { Setting } from "../../utils/types/setting"
+import { IoIosArrowDown } from "react-icons/io"
+import { Hero, HeroContent, HeroText, Star } from "./style"
 
 function LandingPage() {
+  const [settings, setSettings] = useState<Setting>({
+    id: 0,
+    title: "",
+    registerFormUrl: "",
+    youtubeUrl: "",
+    isActive: false,
+    aboutFeContainers: [],
+    timelineEvents: [],
+    photoPreviews: [],
+    qualificationPreviews: [],
+    sponcerContainers: [],
+  })
 
-    const [post, setPost] = useState("");
-    const [imgUrlArray, setImgUrlArray] = useState([]);
-    const [arraySchedule, setArraySchedule] = useState([]);
-    const [qualificationsArray, setQualificationsArray] = useState([]);
-    const [sponsorURLArray, setSponsorURLArray] = useState([]);
+  useEffect(() => {
+    const fetchSetting = async () => {
+      const settings: Setting = await apiClient.getActiveSetting()
+      setSettings(settings)
+    }
+    fetchSetting()
+  }, [])
 
-    useEffect(() => {
-        landingPageInstance.getActiveSetting().then((res) => {
-          setPost(res?.data.aboutFeContainers[0].text)
-          setImgUrlArray(res?.data.photoPreviews.map((e: { imgUrl: string }) => e.imgUrl))
-          setArraySchedule(res?.data.timelineEvents)
-          setQualificationsArray(res?.data.qualificationPreviews.map((e: { text: string; }) => e.text))
-          setSponsorURLArray(res?.data.sponcerContainers.map((e: { imgUrl: string; }) => e.imgUrl))
-        })
-      },[])
-    
-    return(
-        <div>
-            
-            <div className={`top-landing-background`}>
-                <Navbar/>
-            </div>
+  return (
+    <div className="select-none">
+      <img src="https://storage.googleapis.com/fe-camp/setting-header.jpg" alt="Header" className="h-full w-full object-cover" />
+      <Hero className="sm:h-64">
+        <HeroContent className="bg-red-500/0">
+          <Link to={settings.registerFormUrl} className="-translate-y-1/2">
+            <ApplyButton />
+          </Link>
+          <HeroText className="font-BaiJamjuree text-lg font-semibold text-red-1">รู้จักค่าย</HeroText>
+          <AnchorLink href="#main-info-landing">
+            <IoIosArrowDown className="h-12 w-12 animate-pulse text-red-1" />
+          </AnchorLink>
+        </HeroContent>
+      </Hero>
 
-            <div className="top-landing-white-bg">
-                <div className="landing-apply-button">
-                    <Link to={"/register"}><ApplyButton></ApplyButton></Link>
-                </div>
-                <p className="roo-juk-kai-text">รู้จักค่าย</p>
-                <AnchorLink href="#main-info-landing"><img src={icon} alt="icon" className={`icon-image animate-pulse`}></img></AnchorLink>
-            </div>
-            
-            <div className="landing-background" id="main-info-landing">
-                <About post={post}/>
-                <img className="star-icon" src={star} alt="starIcon"></img>
-                <PhotoPreview imgUrlArray={imgUrlArray}/>
-                <VideoCamp/>
-                <img className="star-icon" src={star} alt="starIcon"></img>
-                <Schedule arraySchedule={arraySchedule}/>
-                <img className="star-icon" src={star} alt="starIcon"></img>
-                <Qualifications qualificationsArray={qualificationsArray}/>
-                <img className="star-icon" src={star} alt="starIcon"></img>
-                <Sponsor sponsorURLArray={sponsorURLArray}/>
-            </div>
-        </div>
-    );
+      <div className="bg-landing bg-cover font-BaiJamjuree" id="main-info-landing">
+        <About posts={(settings as Setting).aboutFeContainers} />
+        <Star src={star} alt="starIcon" />
+        <PhotoPreview photoes={(settings as Setting).photoPreviews} />
+        <VideoCamp youtubeUrl={(settings as Setting).youtubeUrl} />
+        <Star src={star} alt="starIcon" />
+        <Schedule schedules={(settings as Setting).timelineEvents} />
+        <Star src={star} alt="starIcon" />
+        <Qualifications qualifications={(settings as Setting).qualificationPreviews} />
+        <Star src={star} alt="starIcon" />
+        <Sponsor sponsors={(settings as Setting).sponcerContainers} />
+      </div>
+    </div>
+  )
 }
 
-export default LandingPage;
+export default LandingPage
