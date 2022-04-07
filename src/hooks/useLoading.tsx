36012c1ee3d230ef6f25beb478aac5f0f7e66304
samithiwat <samithiwat2544@gmail.com>
2022-04-07 18:20:52 +0700

    Fix bug loading in the same path

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -4,9 +4,13 @@ import { useLocation } from "react-router-dom"
 export default function useLoading() {
   const [loading, setLoading] = useState<boolean>(false)
   const location = useLocation()
+  const [path, setPath] = useState<string>("")
 
   useEffect(() => {
-    setLoading(true)
+    if (location.pathname !== path) {
+      setPath(location.pathname)
+      setLoading(true)
+    }
   }, [location])
 
   return { isLoading: loading, setLoading }
