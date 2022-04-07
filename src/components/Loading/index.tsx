3f6f328473013ac97243db2a5b09ee07147656bf
samithiwat <samithiwat2544@gmail.com>
2022-04-07 18:14:16 +0700

    Fix loading margin

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import { CgSpinner } from "react-icons/cg"
 
 const Loading = () => {
   return (
-    <div className="mt-52 flex w-full items-center justify-center">
+    <div className="my-40 flex w-full items-center justify-center sm:my-60">
       <CgSpinner className="z-40 h-52 w-52 animate-spin text-white" />
     </div>
   )
